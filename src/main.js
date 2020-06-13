f9584cacd6301efe51d0a16db90d17b3ba809598
maokaiou <2827017101@qq.com>
2020-06-13 11:37:12 +0800

    路由

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueCookie from 'vue-cookie'
-import store from "./store";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
-Vue.use(VueAxios,axios);
-Vue.use(VueCookie);
 axios.defaults.baseURL = '/api';
 axios.defaults.timeout = 8000; 
+
 
 new Vue({
   router,
